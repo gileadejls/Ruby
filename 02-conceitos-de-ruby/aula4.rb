1bad439d8bee5ea39c14610c8ff0c018d6a59f2e
gileadejls <gileadelima16@gmail.com>
2024-06-03 12:36:14 -0300

    Adicionando parte 2

---
+++ 02-conceitos-de-ruby/aula4.rb
@@ -0,0 +1,10 @@
+#1)saída de dados na tela --> aparecer um comando na tela
+# usuario digite seu nome
+print "Digite seu nome:  "
+
+#2) programa permite que o usuário preencha o que foi pedido
+# gets.chomp
+nome = gets.chomp
+
+#3) Saída final
+puts "Olá, #{nome}. Como vai?"
