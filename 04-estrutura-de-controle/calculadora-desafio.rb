eb42b7923423a04d2b54721103d9e6bc3c194410
gileadejls <gileadelima16@gmail.com>
2024-06-05 12:23:55 -0300

    Calculadora

---
+++ 04-estrutura-de-controle/calculadora-desafio.rb
@@ -0,0 +1,12 @@
+resultado = ''
+
+loop do
+  puts "Digite o primeiro valor: "
+  v1 = gets.chomp.to_f
+  puts"Digite a operação:"
+  puts"[0] Soma [1] Subtração [2] Divisão [3] Multiplicação"
+  operacao = gets.chomp.to_i
+  puts"Digite o segundo valor: "
+  v2 = gets.chomp.to_f
+
+end
